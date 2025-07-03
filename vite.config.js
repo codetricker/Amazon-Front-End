7218ac03e2ae30c399ec3b11e42d54d5ded048a9
Matty <matyshemi@gmail.com>
2025-07-03 11:10:38 -0400

    Fix Vite base config and paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Amazon-Front-End/", // Make sure this exactly matches your repo name, case-sensitive
+  base: "/Amazon-Front-End/",
   plugins: [react()],
 });
