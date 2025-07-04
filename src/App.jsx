93872d88a7cc2f1fb64b9c3a55f666059b7f3511
Matty <matyshemi@gmail.com>
2025-07-04 08:18:31 -0400

    Set Vite base for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,24 @@ import { DataContext } from "./components/DataProvider/DataProvider";
 import AppRouter from "./Routes";
 import { useContext, useEffect } from "react";
 import Type from "./utility/action.type";
-import {auth} from "./utility/firebase"
+import { auth } from "./utility/firebase";
 function App() {
-let [{user}, dispatch]=useContext(DataContext)
-  useEffect (() => {
+  let [{ user }, dispatch] = useContext(DataContext);
+  useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         dispatch({
           type: Type.SET_USER,
-          user: authUser
-       })
+          user: authUser,
+        });
       } else {
         dispatch({
           type: Type.SET_USER,
           user: null,
-        }); 
-     }
-   }) 
-  }, [])
+        });
+      }
+    });
+  }, []);
   return (
     <>
       <AppRouter />
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Amazon-Front-End/",
   plugins: [react()],
+  base: "/Amazon-Front-End/",
 });
