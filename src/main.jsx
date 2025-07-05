715c10a165232fdd6cce935e15067196dc25e821
Matty <matyshemi@gmail.com>
2025-07-04 21:17:14 -0400

    Update router and vite config for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,17 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
-// import { DataProvider } from "./components/DataProvider/DataProvider.jsx";
-// import { reducer, initialState } from "./utility/reducer.js";
-// import { BrowserRouter } from "react-router-dom";
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <DataProvider reducer={reducer} initialState={initialState}>
-//       <BrowserRouter basename="/Amazon-Front-End">
-//         <App />
-//       </BrowserRouter>
-//     </DataProvider>
-//   </StrictMode>
-// );
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { DataProvider } from "./components/DataProvider/DataProvider.jsx";
 import { reducer, initialState } from "./utility/reducer.js";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <DataProvider reducer={reducer} initialState={initialState}>
-      <HashRouter>
+      <BrowserRouter basename="/Amazon-Front-End/">
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </DataProvider>
   </StrictMode>
 );
